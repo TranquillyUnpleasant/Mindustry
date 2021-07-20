7d85776dddc64a68cfea9fd746c322b1106c739a
buthed010203 <naguiar010203@gmail.com>
2021-07-20 08:01:02 -0400

    Better formations
    This is kinda scuffed but whatever

--- core/src/mindustry/ai/formations/patterns/CircleFormation.java
+++ core/src/mindustry/ai/formations/patterns/CircleFormation.java
@@ -10,7 +10,13 @@ public class CircleFormation extends FormationPattern{
 
     @Override
     public Vec3 calculateSlotLocation(Vec3 outLocation, int slotNumber){
-        if(slots > 1){
+        if(slots > 8){ // Custom formation implementation
+            float rows = Mathf.ceil(slots / 8f);
+            float row = slotNumber % rows * .5f + 1;
+            float angle = (360f * (slotNumber / rows)) / (slots / rows);
+            float radius = 52.2625186f; // Same as 8 slots
+            outLocation.set(Angles.trnsx(angle, radius) * row, Angles.trnsy(angle, radius) * row, angle);
+        }else if(slots > 1){
             float angle = (360f * slotNumber) / slots;
             float radius = spacing / (float)Math.sin(180f / slots * Mathf.degRad);
             outLocation.set(Angles.trnsx(angle, radius), Angles.trnsy(angle, radius), angle);
